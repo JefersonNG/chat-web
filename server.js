2900ca56c6f5b5ed9968847aeb9cf83301c234ff
Jeferson Nascimento <tsetegams@gmail.com>
2022-03-21 08:07:06 -0300

    conexao localhost

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.use(express.static(path.join(__dirname, 'docs')));
-app.set('views', path.join(__dirname, 'docs'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
@@ -28,4 +28,4 @@ io.on('connection', socket => {
   });
 });
 
-server.listen('443');
+server.listen(3000);
